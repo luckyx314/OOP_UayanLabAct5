5ab1d53e6cfb038b399f53f049be5b883762e760
Lucky Jones Uayan <luckyjonesbuayan@gmail.com>
2022-12-16 19:41:47 +0800

    Complete LeapYear with Exception Handling

--- src/LeapYear/LeapYearGUI.java
+++ src/LeapYear/LeapYearGUI.java
@@ -1,13 +1,63 @@
 package LeapYear;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+class NegativeYearException extends Exception {
+    @Override
+    public String getMessage() {
+        return "Year cannot be negative. Please try again!";
+    }
+}
+
+class ZeroYearException extends Exception {
+    @Override
+    public String getMessage() {
+        return "Year cannot be zero. Please try again!";
+    }
+}
 
 public class LeapYearGUI extends JFrame{
     private JPanel leapYearPanel;
     private JTextField tfYear;
     private JButton btnCheckYear;
 
+    public LeapYearGUI() {
+        btnCheckYear.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String message = "Not a leap year";
+                int year = 0;
+                try {
+                    try {
+                        year = Integer.parseInt(tfYear.getText());
+
+                        if (year == 0)
+                            throw new ZeroYearException();
+                        if (year < 0)
+                            throw new NegativeYearException();
+
+                        if (leapYearChecker(year))
+                            message = "Leap Year";
+
+                        JOptionPane.showMessageDialog(leapYearPanel, message);
+                    } catch (NumberFormatException err) {
+                        JOptionPane.showMessageDialog(leapYearPanel, "Invalid input. A year should be an integer. Please try again!");
+                    }
+
+                } catch (NegativeYearException | ZeroYearException err) {
+                    JOptionPane.showMessageDialog(leapYearPanel, err.getMessage());
+                }
+            }
+        });
+    }
+
     public JPanel getPnlMain() {
         return leapYearPanel;
+    }
+
+    public boolean leapYearChecker(int year) {
+        return (year%4 == 0 && year%100 != 0) || year %400 == 0;
     }
 }
--- src/LeapYear/LeapYearTestDrive.java
+++ src/LeapYear/LeapYearTestDrive.java
@@ -1,15 +1,15 @@
 package LeapYear;
 
 import javax.swing.*;
+import java.util.Scanner;
 
 public class LeapYearTestDrive {
     public static void main(String[] args) {
         LeapYearGUI app = new LeapYearGUI();
         app.setContentPane(app.getPnlMain());
-        app.setTitle("Leap Year Cheker");
+        app.setTitle("Leap Year Checker");
         app.setSize(300, 300);
         app.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         app.setVisible(true);
     }
-
 }
